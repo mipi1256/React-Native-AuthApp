37ce3abfc1eb23ec56ac3a700757de3c8ded4503
mipi1256 <mipi1258@gmail.com>
2024-06-07 13:24:58 +0900

    #4. 페이지 이동을 위한 라이브러리 다운로드, Stack과 Navigator 작성 중...

--- App.js
+++ App.js
@@ -2,12 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthForm from './components/Auth/AuthForm';
 import AuthContent from './components/Auth/AuthContent';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import { Colors } from './constants/styles';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        // Header 블록에 대한 스타일
+        headerStyle: { backgroundColor: Colors.primary500 },
+        // Header의 텍스트, 버튼 색상
+        headerTintColor: 'white',
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='SignUp' component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AuthContent />
-    </View>
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
   );
 }
 
